ca7b7d35ac93718d03772abe5f99cec4d9ff2092
Aadhavan <aadhavanlenin@gmail.com>
2023-09-12 23:57:09 +0530

    api key updated

--- pages/api/public-api.js
+++ pages/api/public-api.js
@@ -1,21 +1,19 @@
-import { apiKeys } from "../../assets/apiKeys"
-
 export default async function getData() {
 	let stocks = []
 
 	const urls = [
-		'https://api.twelvedata.com/time_series?symbol=JNJ,GOOG,JPM,NVDA,CVX,V,PG&interval=1min&apikey=',
-		'https://api.twelvedata.com/time_series?symbol=HD,LLY,MA,PFE,ABBV,BAC,MRK&interval=1min&apikey=',
-		'https://api.twelvedata.com/time_series?symbol=AAPL,MSFT,AMZN,TSLA,UNH,GOOGL,XOM&interval=1min&apikey=',
-		'https://api.twelvedata.com/time_series?symbol=PEP,KO,COST,META,MCD,WMT,TMO&interval=1min&apikey=',
-		'https://api.twelvedata.com/time_series?symbol=CSCO,DIS,AVGO,WFC,COP,ABT,BMY&interval=1min&apikey=',
-		'https://api.twelvedata.com/time_series?symbol=ACN,DHR,VZ,NEE,LIN,CRM,TXN&interval=1min&apikey=',
-		'https://api.twelvedata.com/time_series?symbol=AMGN,RTX,HON,PM,ADBE,CMCSA,T&interval=1min&apikey=',
+		'https://api.twelvedata.com/time_series?symbol=JNJ,GOOG,JPM,NVDA,CVX,V,PG&interval=1min&apikey=' + process.env.NEXT_PUBLIC_API_1,
+		'https://api.twelvedata.com/time_series?symbol=HD,LLY,MA,PFE,ABBV,BAC,MRK&interval=1min&apikey=' + process.env.NEXT_PUBLIC_API_2,
+		'https://api.twelvedata.com/time_series?symbol=AAPL,MSFT,AMZN,TSLA,UNH,GOOGL,XOM&interval=1min&apikey=' + process.env.NEXT_PUBLIC_API_3,
+		'https://api.twelvedata.com/time_series?symbol=PEP,KO,COST,META,MCD,WMT,TMO&interval=1min&apikey=' + process.env.NEXT_PUBLIC_API_4,
+		'https://api.twelvedata.com/time_series?symbol=CSCO,DIS,AVGO,WFC,COP,ABT,BMY&interval=1min&apikey=' + process.env.NEXT_PUBLIC_API_5,
+		'https://api.twelvedata.com/time_series?symbol=ACN,DHR,VZ,NEE,LIN,CRM,TXN&interval=1min&apikey=' + process.env.NEXT_PUBLIC_API_6,
+		'https://api.twelvedata.com/time_series?symbol=AMGN,RTX,HON,PM,ADBE,CMCSA,T&interval=1min&apikey=' + process.env.NEXT_PUBLIC_API_7,
 	]
 
 	let temp
 	for (let i = 0; i < urls.length; i++) {
-		temp = await fetch(urls[i] + apiKeys[i % 8])
+		temp = await fetch(urls[i])
 		temp = await temp.json()
 		stocks.push(temp)
 	}
